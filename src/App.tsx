bd689dc8e57ced6ece7047f2da9f79f9042c0cc9
John Miller <622498+gokunymbus@users.noreply.github.com>
2022-06-29 16:45:09 -0400

    Fixing indentation and adding theme

--- src/App.tsx
+++ src/App.tsx
@@ -29,43 +29,43 @@ import { PrimaryLink, SecondaryLink } from './components/Buttons';
 const languageData: languageObject = getLanguageData();
 
 const GlobalStyle = createGlobalStyle`
-  body, html, #root {
+    body, html, #root {
     margin: 0;
     padding: 0;
     height: 100%;
     width: 100%;
-  }
+}
 `;
 
 const AppStyled = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 interface AppState {
-  quiz?: IQuizData;
+    quiz?: IQuizData;
 }
 
 class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {}
-  }
+    constructor(props: {}) {
+        super(props);
+        this.state = {}
+    }
 
-  componentDidMount() {
-    const responsePromise = fetchQuizes();
-    responsePromise.then((quizData) => {
-      this.setState({
-        quiz: quizData
-      });
-    })
-  }
-  
-  renderLoading() {
-    return(<Loading />)
-  }
+    componentDidMount() {
+        const responsePromise = fetchQuizes();
+        responsePromise.then((quizData) => {
+            this.setState({
+                quiz: quizData
+            });
+        })
+    }
+
+    renderLoading() {
+        return(<Loading />)
+    }
 
     renderBackToStartButton() {
         const {
@@ -89,83 +89,82 @@ class App extends React.Component<{}, AppState> {
         )
     }
 
-  renderCompleteQuizButton() {
-    const {
-        completeQuizButton
-    } = languageData;
-      return (
-        <PrimaryLink to={AppRoutes.complete}>
-            {completeQuizButton}
-        </PrimaryLink>
-      )
-  }
+    renderCompleteQuizButton() {
+        const {
+            completeQuizButton
+        } = languageData;
+        return (
+            <PrimaryLink to={AppRoutes.complete}>
+                {completeQuizButton}
+            </PrimaryLink>
+        )
+    }
 
-  renderQuiz = () => {
-    const languageCode = getLanguageCode();
-    const {quiz} = this.state;
-    return (
-      <HashRouter>
-        <Routes>
-          <Route
-            path={AppRoutes.home}
-            element={
-              <Home
-                language={languageData}
-                linkTo={`${AppRoutes.question}`}
-                peppers={20}
-              />
-            }
-          />
-          <Route
-            path={`${AppRoutes.question}`}
-            element={
-              <Quiz
-                language={languageData}
-                languageCode={languageCode}
-                data={quiz}
-                onComplete={(answeredQuestions: IAnsweredQuestion[]) => {
-                  setQuizResults(
-                    getQuizResult({
-                      ...quiz!,
-                      answeredQuestions: [...answeredQuestions]
-                    })
-                  );
-                }}
-                renderBackToStartButton={this.renderBackToStartButton()}
-                renderCompleteButton={this.renderCompleteQuizButton()}
-              />
-            }
-          />
-          <Route
-            path={`${AppRoutes.complete}`}
-            element={
-              <Completed
-                language={languageData}
-                languageCode={languageCode}
-                renderPlayAgain={this.renderPlayAgainButton()}
-              />
-            }
-          />
-        </Routes>
-      </HashRouter>
-    )
-  }
+    renderQuiz() {
+        const languageCode = getLanguageCode();
+        const {quiz} = this.state;
+        return (
+            <HashRouter>
+            <Routes>
+                <Route
+                path={AppRoutes.home}
+                element={
+                    <Home
+                    language={languageData}
+                    linkTo={`${AppRoutes.question}`}
+                    peppers={20}
+                    />
+                }
+                />
+                <Route
+                path={`${AppRoutes.question}`}
+                element={
+                    <Quiz
+                    language={languageData}
+                    languageCode={languageCode}
+                    data={quiz}
+                    onComplete={(answeredQuestions: IAnsweredQuestion[]) => {
+                        setQuizResults(
+                        getQuizResult({
+                            ...quiz!,
+                            answeredQuestions: [...answeredQuestions]
+                        })
+                        );
+                    }}
+                    renderBackToStartButton={this.renderBackToStartButton()}
+                    renderCompleteButton={this.renderCompleteQuizButton()}
+                    />
+                }
+                />
+                <Route
+                path={`${AppRoutes.complete}`}
+                element={
+                    <Completed
+                    language={languageData}
+                    languageCode={languageCode}
+                    renderPlayAgain={this.renderPlayAgainButton()}
+                    />
+                }
+                />
+            </Routes>
+            </HashRouter>
+        )
+    }
 
-  render() {
-    const {quiz} = this.state;
-    return (
-        <AppStyled>
-          <ThemeProvider theme={main}>
-            <GlobalStyle />
-            {(quiz)
-                ? this.renderQuiz()
-                : this.renderLoading()
-            }
-            
-          </ThemeProvider>
-        </AppStyled>
-    );
-  }
+    render() {
+        const {quiz} = this.state;
+        return (
+            <AppStyled>
+                <ThemeProvider theme={main}>
+                    <GlobalStyle />
+                    {(quiz)
+                        ? this.renderQuiz()
+                        : this.renderLoading()
+                    }
+                </ThemeProvider>
+            </AppStyled>
+        );
+    }
 }
 
 export default App;
